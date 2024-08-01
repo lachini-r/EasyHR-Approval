d5b021c9f9bd5c04c8f17fc97ca957a27d8182eb
lachini <lachini.vitharanage@student.torrens.edu.au>
2024-08-01 15:09:31 +0530

    Added updates

--- src/main/java/HR/Main.java
+++ src/main/java/HR/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         HRManagement.mainMenu();
-
+        System.out.println("____________________________");
         System.out.println("Feature Z modification");
         System.out.println("Feature X modification");
     }
